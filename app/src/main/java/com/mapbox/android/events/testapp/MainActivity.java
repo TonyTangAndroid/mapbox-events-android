8347fdd3f631209cc8e2fe5903c24a3df91fd3c9
Łukasz Paczos <lukas.paczos@gmail.com>
2018-11-23 14:48:47 +0100

    [test] GoogleLocationEngineImpl leak

--- app/src/main/java/com/mapbox/android/events/testapp/MainActivity.java
+++ app/src/main/java/com/mapbox/android/events/testapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.mapbox.android.events.testapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.android.telemetry.MapboxTelemetry;
@@ -14,6 +23,42 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
   private MapboxTelemetry mapboxTelemetry;
   private PermissionsManager permissionsManager;
 
+  LocationEngine locationEngine;
+  private LocationEngineRequest locationEngineRequest =
+    new LocationEngineRequest.Builder(1000)
+      .setFastestInterval(1000)
+      .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+      .build();
+
+  @NonNull
+  private LocationEngineCallback<LocationEngineResult> currentLocationEngineListener =
+    new LocationEngineCallback<LocationEngineResult>() {
+      @Override
+      public void onSuccess(LocationEngineResult result) {
+        Log.e("LEAK_TEST", "current location: " + result.getLocations());
+      }
+
+      @Override
+      public void onFailure(@NonNull Exception exception) {
+        Log.e("LEAK_TEST", "current location - failure - " + exception);
+      }
+    };
+
+  @NonNull
+  private LocationEngineCallback<LocationEngineResult> lastLocationEngineListener =
+    new LocationEngineCallback<LocationEngineResult>() {
+      @Override
+      public void onSuccess(LocationEngineResult result) {
+        Log.e("LEAK_TEST", "last location: " + result.getLastLocation());
+      }
+
+      @Override
+      public void onFailure(@NonNull Exception exception) {
+        Log.e("LEAK_TEST", "last location - failure - " + exception);
+      }
+    };
+
+  @SuppressLint("MissingPermission")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -32,6 +77,12 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
   }
 
   @Override
+  protected void onStop() {
+    super.onStop();
+    locationEngine.removeLocationUpdates(currentLocationEngineListener);
+  }
+
+  @Override
   protected void onDestroy() {
     super.onDestroy();
     mapboxTelemetry.disable();
@@ -46,11 +97,19 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     boolean permissionsGranted = PermissionsManager.areLocationPermissionsGranted(this);
 
     if (permissionsGranted) {
-      mapboxTelemetry.enable();
+      test();
     } else {
       permissionsManager = new PermissionsManager(this);
       permissionsManager.requestLocationPermissions(this);
     }
+  }
+
+  @SuppressLint("MissingPermission")
+  private void test() {
+    locationEngine = LocationEngineProvider.getBestLocationEngine(this);
+    locationEngine.requestLocationUpdates(locationEngineRequest, currentLocationEngineListener, Looper.getMainLooper());
+//    locationEngine.getLastLocation(lastLocationEngineListener);
+    mapboxTelemetry.enable();
   }
 
   @Override
@@ -58,10 +117,11 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
 
   }
 
+  @SuppressLint("MissingPermission")
   @Override
   public void onPermissionResult(boolean granted) {
     if (granted) {
-      mapboxTelemetry.enable();
+      test();
     }
   }
 }
